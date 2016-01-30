4443dcbff2ed768437f001297f150d630e4cb600
Terry Ott <Terry.Ott@pobox.com>
2016-01-29 20:56:50 -0500

    The reorg that never ended

---
+++ src/OsmFileParser/src/Utf16String.cpp
@@ -0,0 +1,40 @@
+#include <cstdint>
+#include <string>
+#include <utf8.h>
+#include "Utf16String.hpp"
+
+namespace OsmFileParser
+{
+    Utf16String::Utf16String():
+        m_internalUtf16String()
+    {
+        ;
+    }
+
+    Utf16String::~Utf16String()
+    {
+        ;
+    }
+
+    void Utf16String::setFromUtf8Bytes(
+        const std::string&  utf8Bytes )
+    {
+        utf8::utf8to16(utf8Bytes.begin(), utf8Bytes.end(),
+                       std::back_inserter(m_internalUtf16String));
+    }
+
+    std::string Utf16String::toUtf8() const
+    {
+        std::string utf8String;
+
+        utf8::utf16to8(m_internalUtf16String.begin(), m_internalUtf16String.end(),
+                       std::back_inserter(utf8String));
+
+        return utf8String;
+    }
+
+    void Utf16String::clear()
+    {
+        m_internalUtf16String.clear();
+    }
+}
