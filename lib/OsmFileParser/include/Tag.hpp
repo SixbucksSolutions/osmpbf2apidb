17b4a9d15470a897db46d08ce4371e099fbd60da
Terry Ott <Terry.Ott@pobox.com>
2016-01-31 05:56:17 -0500

    Added simple wrapper for primitive tags

---
+++ lib/OsmFileParser/include/Tag.hpp
@@ -0,0 +1,38 @@
+#ifndef _TAG_HPP
+#define _TAG_HPP
+
+#include "Utf16String.hpp"
+
+namespace OsmFileParser
+{
+	namespace OsmPrimitive
+	{
+		class Tag
+		{
+			public:
+				Tag(
+					::OsmFileParser::Utf16String&	key,
+					::OsmFileParser::Utf16String&	value 
+				);
+
+				virtual ~Tag() { }
+
+				::OsmFileParser::Utf16String	getKey() const
+				{
+					return m_key;
+				} 
+
+				::OsmFileParser::Utf16String	getValue() const
+				{
+					return m_value;
+				}
+
+			protected:
+				::OsmFileParser::Utf16String 	m_key;
+				::OsmFileParser::Utf16String	m_value;
+
+		};
+	}
+}
+
+#endif // _TAG_HPP
---
+++ lib/OsmFileParser/src/Tag.cpp
@@ -0,0 +1,18 @@
+#include "Utf16String.hpp"
+#include "Tag.hpp"
+
+namespace OsmFileParser
+{
+	namespace OsmPrimitive
+	{
+		Tag::Tag(
+			::OsmFileParser::Utf16String&   key,
+            ::OsmFileParser::Utf16String&   value ):
+
+			m_key(key),
+			m_value(value)
+		{
+			;
+		}
+	}
+}
