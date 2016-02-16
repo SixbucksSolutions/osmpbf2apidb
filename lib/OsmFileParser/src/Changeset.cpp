e13ec9dc4976c27357e915ffa3249ff9f6c435b1
Terry Ott <Terry.Ott@gmail.com>
2016-02-16 20:16:50 +0000

    Have skeleton for changesets

---
+++ lib/OsmFileParser/src/Changeset.cpp
@@ -0,0 +1,36 @@
+#include <cstdint>
+#include "Primitive.hpp"
+#include "Changeset.hpp"
+
+namespace OsmDataWriter
+{
+    namespace PostgresqlApiDb
+    {
+        Changeset::Changeset(
+            const ::OsmFileParser::OsmPrimitive::Identifier changesetId,
+            const ::OsmFileParser::OsmPrimitive::Identifier userId ) :
+
+            m_changesetId(changesetId),
+            m_userId(userId),
+            m_openedAt(UINT64_MAX),
+            m_closedAt(0),
+            m_changesetChanges(0)
+        {
+            ;
+        }
+
+        void Changeset::updateAccess(
+            const ::OsmFileParser::OsmPrimitive::Timestamp accessTime )
+        {
+            if ( accessTime < m_openedAt )
+            {
+                m_openedAt = accessTime;
+            }
+
+            if ( accessTime > m_closedAt )
+            {
+                m_closedAt = accessTime;
+            }
+        }
+    }
+}
